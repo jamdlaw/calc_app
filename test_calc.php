ad31b6e5968d5b797b17dc7572492a61c641b939
James Lawrence <jamdlaw@yahoo.com>
2016-10-23 19:52:55 -0700

    added test code

--- test_calc.php
+++ test_calc.php
@@ -3,7 +3,11 @@ require('lib/calculator/parse_expression.php');
 //$expression = array ('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ); (should == 18)
 //$expression = array('3', '11', '+' , '5', '-'); (should == 9)
 $calculator = new parse_expression;
-//$calculator->loadExpression(array('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ));
-$result = $calculator->solve(array('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ));
-
+// array('3', '11', '+' , '5', '-'); (should == 9)
+$result = $calculator->solve(array('3', '11', '+' , '5', '-'));
 var_dump($result);
+echo '<br>';
+// array ('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ); (should == 18)
+$result = $calculator->solve(array('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ));
+var_dump($result);
+echo '<br>';
