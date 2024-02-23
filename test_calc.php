0fa5d5d0a0534616db786b7bbc976b87950f2e0e
James Lawrence <jamdlaw@yahoo.com>
2024-02-23 10:44:03 -0800

    added new line characters

--- test_calc.php
+++ test_calc.php
@@ -3,19 +3,17 @@ require('lib/calculator/parse_expression.php');
 $calculator = new parse_expression;
 
 $equation = array_slice($argv, 1);
-//print_r($equation);
 
 $result = $calculator->solve($equation);
 
-var_dump($result);
+print_r($result);
+echo "\n";
 
-//$expression = array ('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ); (should == 18)
-//$expression = array('3', '11', '+' , '5', '-'); (should == 9)
-
-// array('3', '11', '+' , '5', '-'); (should == 9)
-
-//$result = $calculator->solve(array('3', '11', '+' , '5', '-'));
-//var_dump($result);
+$expression = array ('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ); //(should == 18)
+//$expression = array('3', '11', '+' , '5', '-'); //(should == 9)
+$result = $calculator->solve($expression);
+print_r($result);
+echo "\n";
 
 //echo "<br>";
 // array ('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ); (should == 18)
