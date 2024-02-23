68a4ea745c0cd9152077899b9ce72ca726da149e
James Lawrence <jamdlaw@yahoo.com>
2024-02-22 21:38:31 -0800

    get rid of debug code

--- test_calc.php
+++ test_calc.php
@@ -3,18 +3,24 @@ require('lib/calculator/parse_expression.php');
 $calculator = new parse_expression;
 
 $equation = array_slice($argv, 1);
+//print_r($equation);
 
 $result = $calculator->solve($equation);
+
 var_dump($result);
 
 //$expression = array ('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ); (should == 18)
 //$expression = array('3', '11', '+' , '5', '-'); (should == 9)
 
 // array('3', '11', '+' , '5', '-'); (should == 9)
-$result = $calculator->solve(array('3', '11', '+' , '5', '-'));
-var_dump($result);
+
+//$result = $calculator->solve(array('3', '11', '+' , '5', '-'));
+//var_dump($result);
+
 //echo "<br>";
 // array ('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ); (should == 18)
-$result = $calculator->solve(array('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ));
-var_dump($result);
+
+//$result = $calculator->solve(array('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ));
+//var_dump($result);
+
 //echo '<br>';
