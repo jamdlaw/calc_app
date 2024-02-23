45f4fb998b13c4bb0b33410399f868dfd838f537
James Lawrence <jamdlaw@yahoo.com>
2024-02-22 19:14:07 -0800

    started adding code to parse equation from command line

--- test_calc.php
+++ test_calc.php
@@ -1,13 +1,17 @@
 <?php
 require('lib/calculator/parse_expression.php');
+print_r($argv, $equation);
+
+//parse_str(implode('&', array_slice($argv, 1)), $_GET);
+//print_r($_GET);
 //$expression = array ('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ); (should == 18)
 //$expression = array('3', '11', '+' , '5', '-'); (should == 9)
 $calculator = new parse_expression;
 // array('3', '11', '+' , '5', '-'); (should == 9)
 $result = $calculator->solve(array('3', '11', '+' , '5', '-'));
 var_dump($result);
-echo '<br>';
+//echo "<br>";
 // array ('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ); (should == 18)
 $result = $calculator->solve(array('2' , '3' , '11' , '+' ,'5' ,'-' , '*' ));
 var_dump($result);
-echo '<br>';
+//echo '<br>';
